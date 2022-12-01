f70d0b7e3f988c60cfee036ae1db6417d95efa8b
Junhao Zhang <jerry_mountain@hotmail.com>
2022-11-30 20:02:25 -0600

    Initial commit

---
+++ AOC2020/AOC5.java
@@ -0,0 +1,57 @@
+package AOC2020;
+
+import Util.ReadFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AOC5 {
+    public static void main(String[] args) {
+        String f = ReadFile.from(2020, 5);
+        String[] lines = f.split("\\n");
+
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        for (String line : lines) {
+            int lowerRow = 0;
+            int upperRow = 127;
+
+            int lowerCol = 0;
+            int upperCol = 7;
+
+            for (char c : line.toCharArray()) {
+                if (c == 'F') {
+                    // keep lower half
+                    upperRow = lowerRow + (upperRow - lowerRow) / 2;
+                }
+                if (c == 'B') {
+                    // keep upper half
+                    lowerRow = lowerRow + (upperRow + 1 - lowerRow) / 2;
+                }
+                if (c == 'R') {
+                    // keep upper half
+                    lowerCol = lowerCol + (upperCol + 1 - lowerCol) / 2;
+                }
+                if (c == 'L') {
+                    upperCol = lowerCol + (upperCol - lowerCol) / 2;
+                }
+            }
+
+            nums.add(lowerRow * 8 + lowerCol);
+        }
+
+        nums.sort((a, b) -> -a.compareTo(b));
+
+        System.out.println("Part1: " + nums.get(0));
+
+        List<Integer> ns = nums.subList(1, nums.size() - 1);
+        Integer prev = nums.get(0);
+        for (Integer i : ns) {
+            if (prev - i != 1) {
+                // descending
+                System.out.println("Part2: " + (prev - 1));
+            }
+            prev = i;
+        }
+    }
+}
---
+++ Util/ReadFile.java
@@ -0,0 +1,22 @@
+package Util;
+
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class ReadFile {
+    public static String from(int year, int num) {
+        InputStream is = Objects.requireNonNull(ReadFile.class.getClassLoader().getResourceAsStream("AOC" + year + "/AOC" + num + ".txt"));
+        Scanner scan = new Scanner(is);
+
+        StringBuilder out = new StringBuilder();
+        while (scan.hasNextLine()) {
+            out.append(scan.nextLine());
+            out.append("\n");
+        }
+
+        scan.close();
+
+        return out.toString();
+    }
+}
