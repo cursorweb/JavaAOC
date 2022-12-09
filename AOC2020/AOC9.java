0f2597119ec74f413ce7c8b71d4b1078f89dd767
Junhao Zhang <jerry_mountain@hotmail.com>
2022-12-09 15:15:29 -0600

    finished day9
    I went through a few stages of grief because hashCode wasn't working!!!!!
    
    you can't do Object.hash(x, y) because then x,y = y,x

--- AOC2020/AOC9.java
+++ AOC2020/AOC9.java
@@ -5,7 +5,7 @@ import Util.ReadFile;
 public class AOC9 {
     public static void main(String[] args) {
         String file = ReadFile.from(2020, 9);
-        String[] lines = file.split("\\n");
+//        String[] lines = file.split("\\n");
         
         System.out.println(file);
     }
