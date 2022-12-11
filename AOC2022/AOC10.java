1121eec7f1f7466f931e3efd3a0b0af15776e97a
Junhao Zhang <jerry_mountain@hotmail.com>
2022-12-10 20:14:01 -0600

    Day 10 part 2
    Estoy terminado!

--- AOC2022/AOC10.java
+++ AOC2022/AOC10.java
@@ -6,6 +6,7 @@ public class AOC10 {
     private static int cycles = 0;
     private static int sum = 0;
     private static int X = 1;
+    private static final StringBuilder grid = new StringBuilder();
 
     public static void main(String[] args) {
         String file = ReadFile.from(2022, 10);
@@ -26,6 +27,7 @@ public class AOC10 {
         }
         
         System.out.println("Part1: " + sum);
+        System.out.println("Part2:\n" + grid);
     }
 
     private static void startCycle() {
@@ -33,8 +35,20 @@ public class AOC10 {
 
         int c = cycles - 20;
         if (c % 40 == 0) {
-            System.out.println(X + ", " + cycles);
             sum += X * cycles;
         }
+
+        int pos = (cycles - 1) % 40;
+        if (pos == 0) {
+            grid.append("\n");
+        }
+
+        if (Math.abs(X - pos) <= 1) {
+            grid.append("#");
+        } else {
+            grid.append(".");
+        }
+
+//        System.out.println("cycle " + cycles + ", X = " + X + ", POS=" + pos + ", " + grid);
     }
 }
