daf9f955583ed785f586f73d193aa65531ebb72a
Junhao "Jerry" Zhang <junhaodev@gmail.com>
2022-12-14 14:20:00 -0600

    part 1
    oh my god i'm literally pissing myself right now oh my god oh my god oh my god did i seriously just do this in java what the heck java sucks

--- AOC2022/AOC13.java
+++ AOC2022/AOC13.java
@@ -9,24 +9,57 @@ public class AOC13 {
         String file = ReadFile.from(2022, 13);
         String[] lines = file.split("\\n\\n");
 
+        ArrayList<Array[]> arrObjs = new ArrayList<>();
+
         for (String line : lines) {
             String[] arrays = line.split("\n");
             Array left = Array.from(arrays[0].substring(1, arrays[0].length() - 1));
             Array right = Array.from(arrays[1].substring(1, arrays[1].length() - 1));
 
-            System.out.println(left + "\n" + right + "\n");
+            arrObjs.add(new Array[]{left, right});
         }
+
+        int sum = 0;
+        int i = 0;
+        for (Array[] arr : arrObjs) {
+            Array left = arr[0];
+            Array right = arr[1];
+
+            if (left.compare(right) == -1) {
+                sum += i + 1;
+            }
+
+            i++;
+//            System.out.println("\n\n");
+        }
+
+        System.out.println("Part1: " + sum);
     }
 }
 
 
-abstract class Branch { }
+abstract class Branch {
+    // -1 = A < B
+    // 0 = [] -> []??
+    // 1 = A > B
+    public abstract int compare(Branch b);
+}
 
 class Number extends Branch {
     int val;
 
     public Number(int val) {
         this.val = val;
+    }
+
+    @Override
+    public int compare(Branch b) {
+//        System.out.println("Compare " + val + " and " + b);
+        if (b instanceof Number) {
+            return (int) Math.signum(val - ((Number) b).val);
+        } else {
+            return new Array(val).compare(b);
+        }
     }
 
     @Override
@@ -40,6 +73,11 @@ class Array extends Branch {
 
     public Array(ArrayList<Branch> val) {
         this.val = val;
+    }
+
+    public Array(int val) {
+        this.val = new ArrayList<>();
+        this.val.add(new Number(val));
     }
 
     public static Array from(String x) {
@@ -78,6 +116,42 @@ class Array extends Branch {
         }
 
         return new Array(branches);
+    }
+
+    @Override
+    public int compare(Branch bObj) {
+//        System.out.println("Comparing " + val + " " + bObj);
+        if (bObj instanceof Array) {
+            Array b = (Array) bObj;
+            for (int i = 0; i < Math.min(val.size(), b.val.size()); i++) {
+//                System.out.println("Compare " + val.get(i) + " and " + b.val.get(i));
+
+                int result = val.get(i).compare(b.val.get(i));
+
+                if (result == 1) {
+//                    System.out.println("Wrong order");
+                    return 1;
+                }
+
+                if (result == -1) {
+//                    System.out.println("Right order");
+                    return -1;
+                }
+            }
+
+            if (val.size() != b.val.size()) {
+//                System.out.println("Different array size");
+//                System.out.println("So: " + (((int)Math.signum(val.size() - b.val.size()) == -1) ? "Right order" : "Wrong order"));
+                return (int)Math.signum(val.size() - b.val.size());
+            }
+
+//            System.out.println("No order");
+            return 0;
+        } else {
+            Number b = (Number) bObj;
+//            System.out.println("Cast left to array: " + b.val);
+            return compare(new Array(b.val));
+        }
     }
 
     @Override
