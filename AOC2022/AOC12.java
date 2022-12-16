141614df71ebe6d41509d11f88b3d6b37d629681
Junhao "Jerry" Zhang <junhaodev@gmail.com>
2022-12-16 14:00:37 -0600

    remove weird green line on the left side of my gutter whenever I add new stuff
    aoeuhansuhaenotidasnoetuhsa netihansoei haenou haoen iha enid aoenu haoensi daoesn dasnto uhaiadone utha noeidansoeu haeidanoteuh aenoid naoeu tnaoeu aoeu aoei aoeu aoeu aoei ao aoeu aeou aeo iuaeo uaoe ue

--- AOC2022/AOC12.java
+++ AOC2022/AOC12.java
@@ -2,7 +2,7 @@ package AOC2022;
 
 import Util.ReadFile;
 
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class AOC12 {
     public static void main(String[] args) {
@@ -12,19 +12,43 @@ public class AOC12 {
         int height = lines.length;
         int width = lines[0].length();
 
-        char[][] grid = new char[height][width];
+        HashMap<Point, Character> points = new HashMap<>();
 
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                grid[i][j] = lines[i].charAt(j);
+                points.put(new Point(j, i), lines[i].charAt(j));
             }
         }
 
-        System.out.println(Arrays.deepToString(grid));
+        System.out.println(points);
     }
 
     private static
     class Point {
+        int y;
+        int x;
 
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Point point = (Point) o;
+            return y == point.y && x == point.x;
+        }
+
+        @Override
+        public int hashCode() {
+            return (y + "," + x).hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return "(" + x + ", " + y + ")";
+        }
     }
 }
