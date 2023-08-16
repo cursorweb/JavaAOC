9c295f0af5983c292645d2564ffa31efc09a6f47
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2023-08-15 21:52:37 -0500

    refactor

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Python
 AOC solutions in python
+
+## Usage
+```
+aoc [date]
+```
+The date is optional, but `argv` always overrides `main.py`.
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import importlib
+import sys
 
 # change this!
 year = 2022
 day = 3
 
-importlib.import_module(f'y{year}.day{day}.main')
+importlib.import_module(f'y{year}.day{day if len(sys.argv) == 1 else int(sys.argv[1]) }.main')
