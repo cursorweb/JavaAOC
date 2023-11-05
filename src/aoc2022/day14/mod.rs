ebe5fbf9e0e463213d3dc0d7d226c2530c900666
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2023-11-04 20:26:38 -0500

    day14

--- src/aoc2022/day14/mod.rs
+++ src/aoc2022/day14/mod.rs
@@ -4,6 +4,10 @@ use crate::read;
 
 pub fn run() {
     let file = read!();
+
+    let mut highest_y = i32::MIN;
+    let start = (0, 500);
+
     let blocks: HashSet<(i32, i32)> = file
         .flat_map(|line| {
             let pairs = line.split(" -> ");
@@ -39,6 +43,11 @@ pub fn run() {
                     // move it delta times
                     block.0 += sdy;
                     block.1 += sdx;
+
+                    if block.0 > highest_y {
+                        highest_y = block.0;
+                    }
+
                     blocks.push(block);
                 }
             }
@@ -47,16 +56,85 @@ pub fn run() {
         })
         .collect();
 
-    dot(blocks);
+    let floor = highest_y + 2;
+
+    let get_next_pos = |sand: (i32, i32), sands: &mut HashSet<_>| {
+        let mut next_pos = (sand.0 + 1, sand.1);
+
+        if sands.contains(&next_pos) || blocks.contains(&next_pos) {
+            // left
+            let pos = (sand.0 + 1, sand.1 - 1);
+            if !sands.contains(&pos) && !blocks.contains(&pos) {
+                next_pos = pos;
+            } else {
+                // right
+                let pos = (sand.0 + 1, sand.1 + 1);
+                if !sands.contains(&pos) && !blocks.contains(&pos) {
+                    next_pos = pos;
+                } else {
+                    // you must be on top of a sand with no where to go
+                    sands.insert(sand);
+                    return None;
+                }
+            }
+        }
+
+        // you reached the bottom!
+        if next_pos.0 == floor {
+            sands.insert(sand);
+            return None;
+        }
+
+        return Some(next_pos);
+    };
+
+    let mut sands = HashSet::new();
+
+    loop {
+        // (y, x)
+        let mut sand = start;
+        while sand.0 <= highest_y {
+            let Some(next_pos) = get_next_pos(sand, &mut sands) else { break; };
+            sand = next_pos;
+        }
+
+        // went into the 'abyss'
+        if sand.0 > highest_y {
+            break;
+        }
+    }
+
+    println!("Part1: {}", sands.len());
+
+    let mut sands = HashSet::new();
+
+    loop {
+        let mut sand: (i32, i32) = start;
+        loop {
+            let Some(next_pos) = get_next_pos(sand, &mut sands) else { break; };
+            sand = next_pos;
+        }
+
+        // blocked off!
+        if sand == start {
+            break;
+        }
+    }
+
+    println!("Part2: {}", sands.len());
 }
 
-fn dot(blocks: HashSet<(i32, i32)>) {
+fn _dot(blocks: &HashSet<(i32, i32)>, sands: &HashSet<(i32, i32)>) {
     // 9x10
-    for y in 0..=9 {
-        for x in 494..=503 {
+    for y in 0..=11 {
+        for x in 494 - 6..=503 + 6 {
             if x == 500 && y == 0 {
                 print!("+");
+            } else if sands.contains(&(y, x)) {
+                print!("o");
             } else if blocks.contains(&(y, x)) {
+                print!("#");
+            } else if y == 11 {
                 print!("#");
             } else {
                 print!(".");
