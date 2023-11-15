7b277076ea4ee3692b07d051bcc6d31f90416f01
Junhao Zhang <jerry_mountain@hotmail.com>
2023-11-15 16:04:56 -0600

    immensely optimize part1

--- src/aoc2022/day15/mod.rs
+++ src/aoc2022/day15/mod.rs
@@ -1,21 +1,10 @@
-#![allow(unused)]
 use std::collections::{HashMap, HashSet};
 
 use crate::read;
 
-#[derive(Debug)]
-enum Pos {
-    /// The closest beacon
-    Sens((i32, i32)),
-    Beac,
-}
-
-use Pos::*;
-
 pub fn run() {
     let file = read!();
 
-    // let objects: HashMap<(i32, i32), Pos> = file
     // { [sensor]: beacon }
     let objects: HashMap<(i32, i32), (i32, i32)> = file
         .flat_map(|line| {
@@ -31,60 +20,102 @@ pub fn run() {
             let by: i32 = by.split_once("=").unwrap().1.parse().unwrap();
 
             vec![((sy, sx), (by, bx))]
-            // vec![((sy, sx), Sens((by, bx))), ((by, bx), Beac)]
         })
         .collect();
 
-    // number of unavailable on row {row}
-    let mut cols = HashSet::new();
-    let row = 2000000;
-
-    for (sens, beac) in objects {
-        find_used(row, sens, beac, &mut cols);
+    // let row = 10;
+    let row = 2_000_000;
+    let mut ranges = Vec::new();
+    for (&sens, &beac) in &objects {
+        if let Some(range) = row_used(row, sens, beac) {
+            ranges.push(range);
+        }
     }
 
-    println!("{}", cols.len());
-    // dot(&objects);
+    ranges.sort_by(|a, b| a.0.cmp(&b.0));
+
+    let mut new_ranges = Vec::new();
+
+    for &range in &ranges {
+        let mut found = false;
+
+        for nrange in new_ranges.iter_mut() {
+            if intersect(range, *nrange) {
+                *nrange = merge(range, *nrange);
+                found = true;
+                break;
+            }
+        }
+
+        if !found {
+            new_ranges.push(range);
+        }
+    }
+
+    let sum: i32 = new_ranges.into_iter().map(|(a, b)| b - a).sum();
+    println!("Part1: {}", sum);
+
+    /*
+    let mut total = HashSet::new();
+
+    for y in 0..=20 {
+        let mut cols = HashSet::new();
+        for (&sens, &beac) in &objects {
+            find_used(y, sens, beac, &mut cols);
+        }
+        total.extend(cols.into_iter().map(|x| (y, x)));
+    }
+
+    dot(&objects, &total);
+    */
 }
 
 /// sens: (y, x), beac: (y, x)
-fn find_used(row: i32, sens: (i32, i32), beac: (i32, i32), cols: &mut HashSet<i32>) {
-    // -- println!("Reach: ({row}, {}) == {beac:?}", sens.1 - i);
-
-    // this is the distance between the sensor and the beacon
+/// returns (min, max) inclusive
+fn row_used(row: i32, sens: (i32, i32), beac: (i32, i32)) -> Option<(i32, i32)> {
+    // distance between sensor and beacon
     let dist = (sens.1 - beac.1).abs() + (sens.0 - beac.0).abs();
     // after each row, the 'radius' decreases by 1
     let drow = (sens.0 - row).abs();
 
-    // sensor column
-    let scol = sens.1;
+    let sens_col = sens.1;
 
-    // the radius should be inclusive (this is a CLOSED ball!!)
     let radius = (dist - drow).abs();
-
     if (sens.0 - row).abs() <= dist {
-        for i in 0..=radius {
-            // don't count beacons
-            if (row, sens.1 + i) != beac {
-                cols.insert(sens.1 + i);
-            }
-
-            if i > 0 && (row, sens.1 - i) != beac {
-                // this way we don't have to insert 0 twice
-                cols.insert(sens.1 - i);
-            }
-        }
+        Some((sens_col - radius, sens_col + radius))
+    } else {
+        None
     }
 }
 
-fn dot(objects: &HashMap<(i32, i32), Pos>) {
+/// check if 2 ranges intersect
+fn intersect(r1: (i32, i32), r2: (i32, i32)) -> bool {
+    // range2 start > range1 end or range1 start > range2 end
+    return !(r2.0 > r1.1 || r1.0 > r2.1);
+}
+
+/// merge 2 ranges into 1
+fn merge(r1: (i32, i32), r2: (i32, i32)) -> (i32, i32) {
+    let start = r1.0.min(r2.0);
+    let end = r1.1.max(r2.1);
+
+    (start, end)
+}
+
+// [sens]: beac
+fn _dot(objects: &HashMap<(i32, i32), (i32, i32)>, used: &HashSet<(i32, i32)>) {
+    let sensors: HashSet<(i32, i32)> = objects.keys().copied().collect();
+    let beacons: HashSet<(i32, i32)> = objects.values().copied().collect();
     for y in -2..22 {
         for x in -2..25 {
-            let res = objects.get(&(y, x));
-            if let Some(Sens(_)) = res {
+            if sensors.contains(&(y, x)) {
                 print!("S");
-            } else if let Some(Beac) = res {
+            } else if beacons.contains(&(y, x)) {
                 print!("B");
+            } else if used.contains(&(y, x)) {
+                print!("#");
+            } else if y == 11 && x == 14 {
+                print!("0");
             } else {
                 print!(".");
             }
