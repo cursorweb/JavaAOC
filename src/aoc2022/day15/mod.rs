26fdb534206bfeda028e398d62fb0780b0e9ea77
Junhao Zhang <jerry_mountain@hotmail.com>
2023-11-16 10:55:52 -0600

    day 15 part 2

--- src/aoc2022/day15/mod.rs
+++ src/aoc2022/day15/mod.rs
@@ -2,6 +2,12 @@ use std::collections::{HashMap, HashSet};
 
 use crate::read;
 
+// 9104091519600n
+
+const DEBUG: bool = false;
+const ROW: i32 = if DEBUG { 10 } else { 2_000_000 };
+const MAXY: i32 = if DEBUG { 20 } else { 4_000_000 };
+
 pub fn run() {
     let file = read!();
 
@@ -23,10 +29,58 @@ pub fn run() {
         })
         .collect();
 
-    // let row = 10;
-    let row = 2_000_000;
+    /*
     let mut ranges = Vec::new();
     for (&sens, &beac) in &objects {
+        if let Some(range) = row_used(ROW, sens, beac) {
+            ranges.push(range);
+        }
+    }
+
+    ranges.sort_by(|a, b| a.0.cmp(&b.0));
+
+    let mut new_ranges = Vec::new();
+    for range in ranges {
+        let mut found = false;
+
+        for nrange in new_ranges.iter_mut() {
+            if intersect(range, *nrange) {
+                *nrange = merge(range, *nrange);
+                found = true;
+                break;
+            }
+        }
+
+        if !found {
+            new_ranges.push(range);
+        }
+    }
+    */
+    let range = find_ranges(ROW, &objects);
+
+    // there *may* be a chance where the length isn't 1, and may possibly be 2
+    assert_eq!(range.len(), 1);
+    let sum = range[0].1 - range[0].0;
+    // let sum: i32 = new_ranges.into_iter().map(|(a, b)| b - a).sum();
+
+    println!("Part1: {}", sum);
+
+    for row in 0..=MAXY {
+        let range = find_ranges(row, &objects);
+        if range.len() == 2 {
+            // x should be in the middle
+            let x = ((range[0].1 + range[1].0) / 2) as i64;
+            println!("Part2: {}", 4_000_000 * x + row as i64);
+            break;
+        }
+    }
+    // each one has an intersection ...
+}
+
+/// sens: (y, x), beac: (y, x)
+fn find_ranges(row: i32, objects: &HashMap<(i32, i32), (i32, i32)>) -> Vec<(i32, i32)> {
+    let mut ranges = Vec::new();
+    for (&sens, &beac) in objects {
         if let Some(range) = row_used(row, sens, beac) {
             ranges.push(range);
         }
@@ -35,8 +89,7 @@ pub fn run() {
     ranges.sort_by(|a, b| a.0.cmp(&b.0));
 
     let mut new_ranges = Vec::new();
-
-    for &range in &ranges {
+    for range in ranges {
         let mut found = false;
 
         for nrange in new_ranges.iter_mut() {
@@ -52,24 +105,10 @@ pub fn run() {
         }
     }
 
-    let sum: i32 = new_ranges.into_iter().map(|(a, b)| b - a).sum();
-    println!("Part1: {}", sum);
-
-    /*
-    let mut total = HashSet::new();
-
-    for y in 0..=20 {
-        let mut cols = HashSet::new();
-        for (&sens, &beac) in &objects {
-            find_used(y, sens, beac, &mut cols);
-        }
-        total.extend(cols.into_iter().map(|x| (y, x)));
-    }
-
-    dot(&objects, &total);
-    */
+    new_ranges
 }
 
+/// finds the interval that a certain sensor covers up at a row
 /// sens: (y, x), beac: (y, x)
 /// returns (min, max) inclusive
 fn row_used(row: i32, sens: (i32, i32), beac: (i32, i32)) -> Option<(i32, i32)> {
@@ -90,8 +129,12 @@ fn row_used(row: i32, sens: (i32, i32), beac: (i32, i32)) -> Option<(i32, i32)> 
 
 /// check if 2 ranges intersect
 fn intersect(r1: (i32, i32), r2: (i32, i32)) -> bool {
-    // range2 start > range1 end or range1 start > range2 end
-    return !(r2.0 > r1.1 || r1.0 > r2.1);
+    //   |----|
+    // |---|
+    // |--|
+    //     |---|
+    // range2 start - 1 > range1 end or range1 start - 1 > range2 end
+    return !(r2.0 - 1 > r1.1 || r1.0 - 1 > r2.1);
 }
 
 /// merge 2 ranges into 1
