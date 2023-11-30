d22c67e94965be6bb52ef34eb4f6da4913ac0b39
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2023-11-29 21:49:34 -0600

    part 2 of the day 21

--- src/aoc2022/day21/mod.rs
+++ src/aoc2022/day21/mod.rs
@@ -1,18 +1,23 @@
-#![allow(unused)]
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 
 use crate::read;
 
 #[derive(Debug, Clone, Copy)]
+enum OpKind {
+    Plus,
+    Minus,
+    Times,
+    Divide,
+}
+
+#[derive(Debug, Clone, Copy)]
 enum Monkey<'a> {
     Num(i64),
-    Plus(&'a str, &'a str),
-    Minus(&'a str, &'a str),
-    Times(&'a str, &'a str),
-    Divide(&'a str, &'a str),
+    Op(&'a str, OpKind, &'a str),
 }
 
 use Monkey::*;
+use OpKind::*;
 
 pub fn run() {
     let file = read!();
@@ -32,10 +37,10 @@ pub fn run() {
                     let right = ops.next().unwrap().into();
 
                     match op {
-                        "+" => Plus(left, right),
-                        "-" => Minus(left, right),
-                        "*" => Times(left, right),
-                        "/" => Divide(left, right),
+                        "+" => Op(left, Plus, right),
+                        "-" => Op(left, Minus, right),
+                        "*" => Op(left, Times, right),
+                        "/" => Op(left, Divide, right),
                         _ => unreachable!("{op}"),
                     }
                 },
@@ -43,48 +48,98 @@ pub fn run() {
         })
         .collect();
 
-    // println!("Part1: {}", val("root", &graph));
-
-    let mut graph = graph;
+    println!("Part1: {}", val("root", &graph));
 
     // the right seems to NEVER use humn
-    let Plus(left, right) = graph["root"] else { unreachable!("should be +") };
+    let Op(left, _, right) = graph["root"] else { unreachable!("should be +") };
     let right_val = val(right, &graph);
 
-    for i in 0..100 {
-        graph.insert("humn".into(), Num(num1));
-        let y1 = val(left, &graph);
-    }
-
-    /*
-    let num1 = 0;
-    let num2 = 100;
-
-    graph.insert("humn".into(), Num(num1));
-    let y1 = val(left, &graph);
-
-    graph.insert("humn".into(), Num(num2));
-    let y2 = val(left, &graph);
-
-    let slope = (y2 - y1) / (num2 - num1);
-
-    // this is our equation
-    // y1 + slope * x
-    println!("{y1} {slope}x");
-
-    // what is our intercept?
-    let x = (right_val - y1) / slope;
-    */
+    println!("Part2: {}", bfs_solver(left, right_val, &graph));
 }
 
 fn val(name: &str, graph: &HashMap<String, Monkey>) -> i64 {
     match graph[name] {
         Num(n) => n,
-        Plus(a, b) => val(&a, graph) + val(&b, graph),
-        Minus(a, b) => val(&a, graph) - val(&b, graph),
-        Times(a, b) => val(&a, graph) * val(&b, graph),
-        Divide(a, b) => val(&a, graph) / val(&b, graph),
+        Op(a, kind, b) => {
+            let left = val(&a, graph);
+            let right = val(&b, graph);
+            match kind {
+                Plus => left + right,
+                Minus => left - right,
+                Times => left * right,
+                Divide => left / right,
+            }
+        }
     }
+}
+
+struct State<'a> {
+    /// left side of the equation (as a name)
+    name: &'a str,
+    /// right side of the equation
+    expect: i64,
+}
+
+/// equation solver (apply these algorithms, and you'll unwrap to the solution):
+/// ```txt
+/// (...) * 5 = n, (...) = n / 5
+/// (...) / 5 = n, (...) = n * 5
+/// (...) + 5 = n, (...) = n - 5
+/// (...) - 5 = n, (...) = n + 5
+/// ```
+///
+/// NOTE: x only appears once, so a linear equation
+/// NOTE: can use `val` because the base case is reaching `x`
+/// so if the branch reaches x, it won't be used
+///
+/// Use BFS, end case = `humn` on some side
+fn bfs_solver(left: &str, expect: i64, graph: &HashMap<String, Monkey>) -> i64 {
+    let mut queue = VecDeque::new();
+
+    // this stupid code is so stupid
+    // this should be push_front but stupid troll me decides to do push_back
+    queue.push_back(State { name: left, expect });
+
+    while let Some(State { name, expect }) = queue.pop_front() {
+        // base case
+        if name == "humn" {
+            return expect;
+        }
+
+        match graph[name] {
+            Num(_) => {} // not sure what to do here
+            Op(left, op, rname) => {
+                let lval = val(left, &graph);
+                let rval = val(rname, &graph);
+
+                // left [op] right = expect
+                // left = expect ?? [right]
+                queue.push_front(State {
+                    name: left,
+                    expect: match op {
+                        Plus => expect - rval,
+                        Minus => expect + rval,
+                        Times => expect / rval,
+                        Divide => expect * rval,
+                    },
+                });
+
+                // left [op] right = expect
+                // right = expect ?? [left]
+                queue.push_front(State {
+                    name: rname,
+                    expect: match op {
+                        Plus => expect - lval,
+                        Minus => -expect + lval,
+                        Times => expect / lval,
+                        Divide => lval / expect,
+                    },
+                });
+            }
+        }
+    }
+
+    unreachable!("skill issue lmao")
 }
 
 fn _print_equation(name: &str, graph: &HashMap<String, Monkey>) -> String {
@@ -93,24 +148,15 @@ fn _print_equation(name: &str, graph: &HashMap<String, Monkey>) -> String {
     }
     match graph[name] {
         Num(n) => n.to_string(),
-        Plus(a, b) => format!(
-            "({}) + ({})",
+        Op(a, kind, b) => format!(
+            "({}) {} ({})",
             _print_equation(&a, graph),
-            _print_equation(&b, graph)
-        ),
-        Minus(a, b) => format!(
-            "({}) - ({})",
-            _print_equation(&a, graph),
-            _print_equation(&b, graph)
-        ),
-        Times(a, b) => format!(
-            "({}) * ({})",
-            _print_equation(&a, graph),
-            _print_equation(&b, graph)
-        ),
-        Divide(a, b) => format!(
-            "({}) / ({})",
-            _print_equation(&a, graph),
+            match kind {
+                Plus => "+",
+                Minus => "-",
+                Times => "*",
+                Divide => "/",
+            },
             _print_equation(&b, graph)
         ),
     }
