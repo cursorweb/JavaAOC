fe6b7c7ac9e416cd20a89cc648acc0280eaa84b0
Junhao Zhang <jerry_mountain@hotmail.com>
2024-01-02 10:25:52 -0600

    remove data.txt

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import importlib
 import sys
 
 # change this!
-year = 2022
-day = 3
+year = 2023
+day = 1
 
-importlib.import_module(f'y{year}.day{day if len(sys.argv) == 1 else int(sys.argv[1]) }.main')
+importlib.import_module(
+    f"y{year}.day{day if len(sys.argv) == 1 else int(sys.argv[1]) }.main"
+)
