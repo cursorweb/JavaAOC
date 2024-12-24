490a0290cab9f1eae732165556e2e532d3d6f219
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2024-12-24 14:44:46 -0600

    day11

---
+++ aoc2024/day11/main.py
@@ -0,0 +1,45 @@
+import math
+from read import read
+
+nums = [int(x) for x in read(False).split(" ")]
+
+nums_temp = nums.copy()
+
+
+def count_digits(x: int):
+    return int(math.log10(x) + 1)
+
+
+def split_digits(x: int):
+    digs = count_digits(x) / 2
+    left = x // (10**digs)
+    right = x % (10**digs)
+    return int(left), int(right)
+
+
+cache = {}
+
+
+def count_times(num, it=75):
+    if it == 0:
+        return 1
+    if (num, it) in cache:
+        return cache[num, it]
+
+    if num == 0:
+        out = count_times(1, it - 1)
+    elif count_digits(num) % 2 == 0:
+        l, r = split_digits(num)
+        out = count_times(l, it - 1) + count_times(r, it - 1)
+    else:
+        out = count_times(num * 2024, it - 1)
+
+    cache[num, it] = out
+    return out
+
+
+part1 = sum([count_times(n, 25) for n in nums])
+part2 = sum([count_times(n) for n in nums])
+
+print("part1:", part1)
+print("part2:", part2)
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import importlib
 import sys
 
 # change this!
-year = 2023
-day = 1
+year = 2024
+day = 11
 
 importlib.import_module(
     f"aoc{year}.day{day if len(sys.argv) == 1 else int(sys.argv[1]) }.main"
