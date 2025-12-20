61486b15e73a2c8f3d4756858c81e42b94b4d625
Junhao Zhang <jerry_mountain@hotmail.com>
2025-12-20 16:00:12 -0600

    AOC DAY 7 6 7 6 7 😜

--- src/aoc2025/day7/mod.rs
+++ src/aoc2025/day7/mod.rs
@@ -9,7 +9,9 @@ pub fn run() {
 
     let start = file.next().unwrap().chars().position(|c| c == 'S').unwrap();
 
-    let rest = file.skip(1).step_by(2);
+    let width = file.next().unwrap().len();
+
+    let rest = file.step_by(2);
 
     let splitters = rest
         .map(|x| {
@@ -20,11 +22,14 @@ pub fn run() {
         .collect_vec();
 
     let mut beams = HashSet::from([start]);
+    let mut beams_dup = vec![0; width];
+    beams_dup[start] = 1;
 
     let mut splits = 0;
 
     for row in splitters {
         let mut next = HashSet::new();
+        let mut next_dup = vec![0; width];
 
         for beam in beams {
             if row.contains(&beam) {
@@ -36,9 +41,33 @@ pub fn run() {
             }
         }
 
+        for (beam, &count) in beams_dup.iter().enumerate() {
+            if row.contains(&beam) {
+                next_dup[beam - 1] += count;
+                next_dup[beam + 1] += count;
+            } else {
+                next_dup[beam] += count;
+            }
+        }
+
+        // println!("{next_dup:?}");
+        // for i in 0..next_dup.len() {
+        //     if next_dup[i] != 0 {
+        //         assert!(!row.contains(&i));
+        //         print!("({})", next_dup[i]);
+        //     } else if row.contains(&i) {
+        //         print!("^");
+        //     } else {
+        //         print!(".");
+        //     }
+        // }
+        // println!();
+        // input!();
+
         beams = next;
+        beams_dup = next_dup;
     }
 
     println!("Part1: {splits}");
-    println!("Part2: {}", (beams.len() + 1) * (splits - 1));
+    println!("Part2: {}", beams_dup.iter().sum::<i64>());
 }
